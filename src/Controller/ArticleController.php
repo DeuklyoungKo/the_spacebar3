53e52390458cd0c44b82396abdfddfb841246da4
DeuklyoungKo <deutschgon@gmail.com>
2019-01-06 22:36:36 +0000

    chapter 2-2

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,24 +8,37 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response("OMG! My first page already! WOO!");
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}/heart", name="article_toggle_heart",methods={"POST"})
      */
-    public function show($slug)
+    public function togglearticleHeart($slug, LoggerInterface $logger)
+    {
+
+        $logger->info('Article is being hearted!');
+        return new JsonResponse(['hearts' => rand(5,100)]);
+    }
+
+    /**
+     * @Route("/news/{slug}", name="article_show")
+     */
+    public function show($slug, Environment $twigEnvironment)
     {
 
 //        dump($slug, $this);
@@ -38,9 +51,13 @@ class ArticleController extends AbstractController
 
 
 //        return new Response(sprintf('"Future page to show one space article: "%s"',$slug));
-        return $this->render('article/show.html.twig',[
+//        return $this->render('article/show.html.twig',[
+        $html = $twigEnvironment->render('article/show.html.twig',[
             'title' => ucwords(str_replace('-', ' ',$slug)),
             'comments' => $comments,
+            'slug' => $slug,
         ]);
+
+        return new Response($html);
     }
 }
