0141135deab63512a9b4f100ee6600c3db73158e
DeuklyoungKo <deutschgon@gmail.com>
2019-01-07 14:35:35 +0000

    chapter 2-13

---
+++ public/etc/info.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 2019-01-07
+ * Time: 오전 1:01
+ */
+
+phpinfo();
+?>
--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -8,15 +8,25 @@
 
 namespace App\Controller;
 
+use App\Service\MarkdownHelper;
 use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
 
 class ArticleController extends AbstractController
 {
+    /**
+     * @var
+     */
+    private $isDebug;
+
+    public function __construct(bool $isDebug)
+    {
+        dump($isDebug);die;
+        $this->isDebug = $isDebug;
+    }
+
     /**
      * @Route("/", name="app_homepage")
      */
@@ -38,10 +48,9 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug, Environment $twigEnvironment)
+    public function show($slug, MarkdownHelper $markdownHelper)
     {
 
-//        dump($slug, $this);
 
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
@@ -49,15 +58,32 @@ class ArticleController extends AbstractController
             'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
 
+        $articleContent = <<<EOF
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
+**turkey** shank eu pork belly meatball non cupim.
+Laboris beef ribs fatback fugiat eiusmod jowl kielbasa alcatra dolore velit ea ball tip. Pariatur
+laboris sunt venison, et laborum dolore minim non meatball. Shankle eu flank aliqua shoulder,
+capicola biltong frankfurter boudin cupim officia. Exercitation fugiat consectetur ham. Adipisicing
+picanha shank et filet mignon pork belly ut ullamco. Irure velit turducken ground round doner incididunt
+occaecat lorem meatball prosciutto quis strip steak.
+Meatball adipisicing ribeye bacon strip steak eu. Consectetur ham hock pork hamburger enim strip steak
+mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incididunt in filet mignon
+strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
+cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
+fugiat.
+EOF;
 
-//        return new Response(sprintf('"Future page to show one space article: "%s"',$slug));
-//        return $this->render('article/show.html.twig',[
-        $html = $twigEnvironment->render('article/show.html.twig',[
+
+        $articleContent = $markdownHelper->parse($articleContent);
+
+        return $this->render('article/show.html.twig',[
             'title' => ucwords(str_replace('-', ' ',$slug)),
             'comments' => $comments,
             'slug' => $slug,
+            'articleContent' => $articleContent,
         ]);
 
-        return new Response($html);
     }
 }
