8649a2b94f870320919d24fe49d423da724ced71
DeuklyoungKo <deutschgon@gmail.com>
2019-01-27 14:07:45 +0100

    for chapter 5-14

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,15 +4,36 @@ namespace App\Controller;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
 
 class TestController extends AbstractController
 {
+
+    use TargetPathTrait;
+
     /**
      * @Route("/test", name="test")
      */
-    public function test(UserPasswordEncoderInterface $encoder)
+    public function test(Request $request)
+    {
+
+        $session = $request->getSession();
+
+        dump($session);
+
+        $target = $this->getTargetPath($request->getSession(),'main');
+        dd($target);
+
+    }
+
+    /**
+     * @Route("/test/UserPassword", name="test_UserPassword")
+     */
+    public function testUserPassword(UserPasswordEncoderInterface $encoder)
     {
         $user  = new User();
 
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -3,7 +3,6 @@
 namespace App\Security;
 
 use App\Repository\UserRepository;
-use Symfony\Component\Finder\Tests\Iterator\RealIteratorTestCase;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RouterInterface;
@@ -16,10 +15,12 @@ use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
 use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
 class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 {
 
+    use TargetPathTrait;
     /**
      * @var UserRepository
      */
@@ -84,6 +85,12 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
+        if($targetPath = $this->getTargetPath($request->getSession(),$providerKey)){
+
+            dd($providerKey);
+
+            return new RedirectResponse($targetPath);
+        }
         return new RedirectResponse($this->router->generate('app_homepage'));
     }
 
