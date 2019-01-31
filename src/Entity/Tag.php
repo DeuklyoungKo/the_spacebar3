9e982f3199bf9edf1fd82b04f3866450e98e166b
DeuklyoungKo <deutschgon@gmail.com>
2019-01-31 17:53:08 +0100

    for chapter 6-8

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -101,4 +101,6 @@ class Tag
 
         return $this;
     }
+
 }
+
--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -14,11 +16,18 @@ class ArticleFormType extends AbstractType
     {
         $builder
             ->add('title',TextType::class, [
-                'help' => 'Choose something catchy!'
+                'help' => 'Choose something catchy!',
             ])
             ->add('content')
             ->add('publishedAt',null,[
-                'widget' => 'single_text'
+                'widget' => 'single_text',
+            ])
+            ->add('author', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => function(User $user) {
+                    return sprintf('(%d) %s', $user->getId(), $user->getEmail());
+                },
+                'placeholder' => 'Choose an author'
             ])
 //            ->add('slug')
 //            ->add('heartCount')
