1bbd71638e2866d770576ab33e1a1b8a45fb8ffd
DeuklyoungKo <deutschgon@gmail.com>
2019-02-04 18:08:25 +0100

    for chapter 6-36

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -76,6 +76,17 @@ class Article
     private $author;
 
     /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $location;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $specificLocationName;
+
+
+    /**
      * @param ArrayCollection $tags
      */
     public function setTags(ArrayCollection $tags): void
@@ -277,6 +288,30 @@ class Article
                 ->atPath('title')
                 ->addViolation();
         }
+    }
+
+    public function getLocation(): ?string
+    {
+        return $this->location;
+    }
+
+    public function setLocation(string $location): self
+    {
+        $this->location = $location;
+
+        return $this;
+    }
+
+    public function getSpecificLocationName(): ?string
+    {
+        return $this->specificLocationName;
+    }
+
+    public function setSpecificLocationName(string $specificLocationName): self
+    {
+        $this->specificLocationName = $specificLocationName;
+
+        return $this;
     }
 
 }
